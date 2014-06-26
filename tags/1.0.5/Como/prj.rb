67f05e65bb4ac5284f7882cf3d152c3801a15579
igor.mironchik@gmail.com <igor.mironchik@gmail.com@e8efb442-a3ef-2481-d551-9cec42c42fea>
2014-06-26 10:34:20 +0000

    Tag 1.0.5

---
+++ tags/1.0.5/Como/prj.rb
@@ -0,0 +1,28 @@
+
+require 'mxx_ru/cpp/qt5'
+
+Mxx_ru::Cpp::lib_target {
+
+    rtl_mode( Mxx_ru::Cpp::RTL_SHARED )
+	rtti_mode( Mxx_ru::Cpp::RTTI_ENABLED )
+	threading_mode( Mxx_ru::Cpp::THREADING_MULTI )
+
+	target( "lib/Como" )
+	
+	qt = generator( MxxRu::Cpp::Qt5.new( self ) )
+	qt.use_modules( QT_CORE, QT_NETWORK )
+
+	sources_root( "private" ) {
+		cpp_source( "buffer.cpp" )
+		cpp_source( "messages.cpp" )
+		cpp_source( "protocol.cpp" )
+	}
+	
+	qt.h2moc( "client_socket.hpp" )
+	qt.h2moc( "server_socket.hpp" )
+	qt.h2moc( "source.hpp" )
+
+	cpp_source( "client_socket.cpp" )
+	cpp_source( "server_socket.cpp" )
+	cpp_source( "source.cpp" )
+}
---
+++ tags/1.0.5/samples/server/Server.pro
@@ -0,0 +1,19 @@
+
+TEMPLATE = app
+TARGET = Server
+DESTDIR = ../..
+DEPENDPATH += . ../..
+INCLUDEPATH += . ../..
+CONFIG += console qt
+QT += network
+QT -= gui
+LIBS += -L../../lib -lComo
+
+HEADERS += first_thread.hpp \
+           second_thread.hpp \
+           third_thread.hpp
+
+SOURCES += first_thread.cpp \
+           second_thread.cpp \
+           third_thread.cpp \
+           main.cpp
