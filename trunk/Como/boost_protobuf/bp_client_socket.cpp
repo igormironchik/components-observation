d866906c904cf85d33672ab4c4a9c02b35c809cb
igor.mironchik@gmail.com <igor.mironchik@gmail.com@e8efb442-a3ef-2481-d551-9cec42c42fea>
2014-09-01 18:00:04 +0000

    Fixed issue with boost::asio::buffer

--- trunk/Como/boost_protobuf/bp_client_socket.cpp
+++ trunk/Como/boost_protobuf/bp_client_socket.cpp
@@ -42,6 +42,7 @@
 #include <vector>
 #include <sstream>
 #include <atomic>
+#include <algorithm>
 
 // boost include.
 #include <boost/asio.hpp>
@@ -79,125 +80,14 @@ static const std::uint16_t c_sourceMessageType = 0x0002;
 static const std::uint16_t c_deinitSourceMessageType = 0x0003;
 
 
-//
-// ClientSocketPrivate
-//
-
-class ClientSocketPrivate {
-public:
-	ClientSocketPrivate( boost::asio::io_service & io,
-		ServerSocket * server )
-		:	m_socket( io )
-		,	m_server( server )
-	{
-	}
-
-	//! \return Parsed digit.
-	inline std::uint16_t parseDigit( const char * str )
-	{
-		return ( std::uint16_t ) str[ 0 ] * 256 + ( std::uint16_t ) str[ 1 ];
-	}
-
-	//! Parse message's header and notify server.
-	bool parseHeaderAndNotifyServer( const char * header,
-		std::shared_ptr< ClientSocket > socket )
-	{
-		if( std::strncmp( c_magicNumber1, header, 8 ) == 0 )
-		{
-			const std::uint16_t type = parseDigit( header + 8 );
-			const std::uint16_t length = parseDigit( header + 10 );
-
-			if( c_getListOfSourcesMessageType == type )
-				m_server->sendListOfSources( socket );
-
-			if( length > 0 )
-				readMessage( length, socket );
-
-			return true;
-		}
-		else
-		{
-			m_server->disconnection( socket );
-
-			return false;
-		}
-	}
-
-	//! Read message.
-	void readMessage( std::size_t length,
-		std::shared_ptr< ClientSocket > socket )
-	{
-		std::vector< char > message( length );
-
-		boost::asio::async_read( m_socket,
-			boost::asio::buffer( message.data(), length ),
-			[ this, socket ] ( boost::system::error_code error, std::size_t )
-				{
-					if( error )
-						m_server->disconnection( socket );
-				}
-		);
-	}
-
-	//! Socket.
-	tcp::socket m_socket;
-	//! Server socket.
-	ServerSocket * m_server;
-	//! Stopped flag
-	std::atomic_flag m_isStopped;
-	//! Bufer.
-	char m_header[ c_headerSize ];
-}; // class ClientSocketPrivate;
-
-
-//
-// ClientSocket
-//
-
-ClientSocket::ClientSocket( boost::asio::io_service & io,
-	ServerSocket * server )
-	:	d( new ClientSocketPrivate( io, server ) )
+static inline void write16BitNumber( std::ostringstream & stream,
+	std::uint16_t number )
 {
-}
+	const char low = number & 0xFF;
+	const char hight = ( number >> 8 ) & 0xFF;
 
-ClientSocket::~ClientSocket()
-{
-}
-
-tcp::socket &
-ClientSocket::socket()
-{
-	return d->m_socket;
-}
-
-void
-ClientSocket::start( std::shared_ptr< ClientSocket > socket )
-{
-	if( d->m_socket.is_open() )
-	{
-		boost::asio::async_read( d->m_socket,
-			boost::asio::buffer( d->m_header, c_headerSize ),
-			[ this, socket ] ( boost::system::error_code error, std::size_t )
-				{
-					if( error )
-						d->m_server->disconnection( socket );
-					else
-					{
-						if( d->parseHeaderAndNotifyServer( d->m_header, socket ) &&
-							d->m_socket.is_open() )
-								start( socket );
-					}
-				}
-		);
-	}
-}
-
-void
-ClientSocket::stop( std::shared_ptr< ClientSocket > socket )
-{
-	if( !d->m_isStopped.test_and_set( std::memory_order_acquire ) )
-		socket->socket().close();
-}
+	stream << hight << low;
+} // write16BitNumber
 
 static inline std::string anyToString( const boost::any & value,
 	Source::Type type )
@@ -261,48 +151,179 @@ static inline std::string anyToString( const boost::any & value,
 	{
 		return std::string();
 	}
+} // anyToString
+
+
+//
+// ClientSocketPrivate
+//
+
+class ClientSocketPrivate {
+public:
+	ClientSocketPrivate( boost::asio::io_service & io,
+		ServerSocket * server )
+		:	m_socket( io )
+		,	m_server( server )
+	{
+	}
+
+	//! \return Parsed digit.
+	inline std::uint16_t parseDigit( const char * str )
+	{
+		return ( std::uint16_t ) str[ 0 ] * 256 + ( std::uint16_t ) str[ 1 ];
+	}
+
+	//! Parse message's header and notify server.
+	bool parseHeaderAndNotifyServer( const char * header,
+		std::shared_ptr< ClientSocket > socket )
+	{
+		if( std::strncmp( c_magicNumber1, header, 8 ) == 0 )
+		{
+			const std::uint16_t type = parseDigit( header + 8 );
+			const std::uint16_t length = parseDigit( header + 10 );
+
+			if( c_getListOfSourcesMessageType == type )
+				m_server->sendListOfSources( socket );
+
+			if( length > 0 )
+				readMessage( length, socket );
+
+			return true;
+		}
+		else
+		{
+			m_server->disconnection( socket );
+
+			return false;
+		}
+	}
+
+	//! Read message.
+	void readMessage( std::size_t length,
+		std::shared_ptr< ClientSocket > socket )
+	{
+		std::vector< char > buf( length, 0x00u );
+		m_readBuffers.emplace_back( buf );
+
+		boost::asio::async_read( m_socket,
+			boost::asio::buffer( m_readBuffers.back().data(), length ),
+			[ this, socket ] ( boost::system::error_code error, std::size_t )
+				{
+					if( error )
+						m_server->disconnection( socket );
+					else
+						m_readBuffers.pop_front();
+				}
+		);
+	}
+
+	//! Write message.
+	void writeMessage( const Source & source,
+		std::uint16_t type,
+		std::shared_ptr< ClientSocket > socket )
+	{
+		ComoMessage msg;
+		msg.set_type( source.type() );
+		msg.set_name( source.name() );
+		msg.set_typename_( source.typeName() );
+		msg.set_datetime( boost::posix_time::to_iso_extended_string(
+			source.dateTime() ) );
+		msg.set_description( source.description() );
+		msg.set_value( anyToString( source.value(), source.type() ) );
+
+		const std::string strMessage = msg.SerializeAsString();
+
+		std::ostringstream stream;
+		stream.write( c_magicNumber2, 8 );
+		write16BitNumber( stream, type );
+		write16BitNumber( stream, ( std::uint16_t ) strMessage.length() );
+
+		const std::string data = stream.str() + strMessage;
+
+		std::vector< char > buf( data.length(), 0x00u );
+		std::copy( data.begin(), data.end(), buf.begin() );
+		m_writeBuffers.emplace_back( buf );
+
+		boost::asio::async_write( socket->socket(),
+			boost::asio::buffer( m_writeBuffers.back().data(), data.length() ),
+			[ this, socket ] ( boost::system::error_code error, std::size_t )
+				{
+					if( error )
+						m_server->disconnection( socket );
+					else
+						m_writeBuffers.pop_front();
+				}
+		);
+	}
+
+	//! Socket.
+	tcp::socket m_socket;
+	//! Server socket.
+	ServerSocket * m_server;
+	//! Stopped flag
+	std::atomic_flag m_isStopped;
+	//! Read bufer.
+	std::list< std::vector< char > > m_readBuffers;
+	//! Write bufer.
+	std::list< std::vector< char > > m_writeBuffers;
+}; // class ClientSocketPrivate;
+
+
+//
+// ClientSocket
+//
+
+ClientSocket::ClientSocket( boost::asio::io_service & io,
+	ServerSocket * server )
+	:	d( new ClientSocketPrivate( io, server ) )
+{
 }
 
-static inline void write16BitNumber( std::ostringstream & stream,
-	std::uint16_t number )
+ClientSocket::~ClientSocket()
 {
-	char low = number & 0xFF;
-	char hight = ( number >> 8 ) & 0xFF;
-
-	stream << hight << low;
 }
 
-static inline void writeMessage( const Source & source,
-	std::uint16_t type,
-	ServerSocket * server,
-	std::shared_ptr< ClientSocket > socket )
+tcp::socket &
+ClientSocket::socket()
 {
-	ComoMessage msg;
-	msg.set_type( source.type() );
-	msg.set_name( source.name() );
-	msg.set_typename_( source.typeName() );
-	msg.set_datetime( boost::posix_time::to_iso_extended_string(
-		source.dateTime() ) );
-	msg.set_description( source.description() );
-	msg.set_value( anyToString( source.value(), source.type() ) );
+	return d->m_socket;
+}
 
-	const std::string strMessage = msg.SerializeAsString();
+void
+ClientSocket::start( std::shared_ptr< ClientSocket > socket )
+{
+	if( d->m_socket.is_open() )
+	{
+		std::vector< char > buf( c_headerSize, 0x00u );
+		d->m_readBuffers.emplace_back( buf );
 
-	std::ostringstream stream;
-	stream.write( c_magicNumber2, 8 );
-	write16BitNumber( stream, type );
-	write16BitNumber( stream, ( std::uint16_t ) strMessage.length() );
+		boost::asio::async_read( d->m_socket,
+			boost::asio::buffer( d->m_readBuffers.back().data(), c_headerSize ),
+			[ this, socket ] ( boost::system::error_code error, std::size_t )
+				{
+					if( error )
+						d->m_server->disconnection( socket );
+					else
+					{
+						const bool headerParsed =
+							d->parseHeaderAndNotifyServer(
+								d->m_readBuffers.front().data(), socket );
 
-	const std::string data = stream.str() + strMessage;
+						d->m_readBuffers.pop_front();
 
-	boost::asio::async_write( socket->socket(),
-		boost::asio::buffer( data.data(), data.length() ),
-		[ server, socket ] ( boost::system::error_code error, std::size_t )
-			{
-				if( error )
-					server->disconnection( socket );
-			}
-	);
+						if( headerParsed &&	d->m_socket.is_open() )
+							start( socket );
+					}
+				}
+		);
+	}
+}
+
+void
+ClientSocket::stop( std::shared_ptr< ClientSocket > socket )
+{
+	if( !d->m_isStopped.test_and_set( std::memory_order_acquire ) )
+		socket->socket().close();
 }
 
 void
@@ -310,8 +331,7 @@ ClientSocket::sendSourceMessage( const Source & source,
 	std::shared_ptr< ClientSocket > socket )
 {
 	if( d->m_socket.is_open() )
-		writeMessage( source, c_sourceMessageType,
-			d->m_server, socket );
+		d->writeMessage( source, c_sourceMessageType, socket );
 }
 
 void
@@ -319,8 +339,7 @@ ClientSocket::sendDeinitSourceMessage( const Source & source,
 	std::shared_ptr< ClientSocket > socket )
 {
 	if( d->m_socket.is_open() )
-		writeMessage( source, c_deinitSourceMessageType,
-			d->m_server, socket );
+		d->writeMessage( source, c_deinitSourceMessageType, socket );
 }
 
 } /* namespace BoostProtobuf */
